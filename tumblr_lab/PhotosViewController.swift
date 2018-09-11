8303f863e31417bb5f485ad6331f6a9df64f7675
user144860 <user144860@telehouse-moboware.dmarc.lga11.atlanticmetro.net>
2018-09-11 14:00:48 -0400

    I added the avatar

--- tumblr_lab/PhotosViewController.swift
+++ tumblr_lab/PhotosViewController.swift
@@ -12,7 +12,7 @@ import AlamofireImage
 class PhotosViewController: UIViewController, UITableViewDelegate,UITableViewDataSource {
     
     var image: UIImage!
-
+    
     var posts: [[String: Any]] = []
     var refreshControl: UIRefreshControl!
     
@@ -83,14 +83,45 @@ class PhotosViewController: UIViewController, UITableViewDelegate,UITableViewDat
     }
     
     
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return posts.count
+    }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return posts.count
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let headerView = UIView(frame: CGRect(x: 0, y: 0, width: 320, height: 50))
+        headerView.backgroundColor = UIColor(white: 1, alpha: 0.9)
+        
+        let profileView = UIImageView(frame: CGRect(x: 10, y: 10, width: 30, height: 30))
+        profileView.clipsToBounds = true
+        profileView.layer.cornerRadius = 15;
+        profileView.layer.borderColor = UIColor(white: 0.7, alpha: 0.8).cgColor
+        profileView.layer.borderWidth = 1;
+        
+        // Set the avatar
+        profileView.af_setImage(withURL: URL(string: "https://api.tumblr.com/v2/blog/humansofnewyork.tumblr.com/avatar")!)
+        headerView.addSubview(profileView)
+        
+        // Add a UILabel for the date here
+        //let post = posts[indexPath.section]
+        //let label = post["date"]
+        //dateLabel.text = label
+        // Use the section number to get the right URL
+        // let label = ...
+        
+        return headerView
+        
+    }
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 30
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "PhotoCell", for: indexPath) as! PhotoCell
-        let post = posts[indexPath.row]
+        let post = posts[indexPath.section]
         
         // 1.            // 2.          // 3.
         if let photos = post["photos"] as? [[String: Any]] {
